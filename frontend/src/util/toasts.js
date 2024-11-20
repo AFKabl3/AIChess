f2a6bde9d267e12c6176c521722d5e3ba32eca4d
noelinson <151772977+noelinson@users.noreply.github.com>
2024-11-20 16:38:16 +0100

    Add files via upload

--- frontend/src/util/toasts.js
+++ frontend/src/util/toasts.js
@@ -1,11 +1,6 @@
 import toast from 'react-hot-toast';
 
 export const waitForResponseToast = () =>
-  toast('Please wait for a response from the last move', {
-    icon: '⏳',
-  });
-
-export const waitForResponseQuestionToast = () =>
-  toast('Please wait for a response for the previous question', {
+  toast('Please wait for a response from the assistant', {
     icon: '⏳',
   });
