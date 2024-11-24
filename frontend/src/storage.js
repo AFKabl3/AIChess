f2c32ccc296a9ecb8ecef94d0d0731c8549e9e80
thorsjur <thorsj00@hotmail.com>
2024-11-24 17:45:03 +0100

    feat: setup global config
    Probably should have done this more React friendly, but oh well.

---
+++ frontend/src/storage.js
@@ -0,0 +1,32 @@
+const store = (key, value) => {
+  const storage = JSON.parse(localStorage.getItem('chess-storage')) || {};
+  storage[key] = value;
+  localStorage.setItem('chess-storage', JSON.stringify(storage));
+};
+
+const read = (key) => {
+  const storage = JSON.parse(localStorage.getItem('chess-storage')) || {};
+  return storage[key] || null;
+};
+
+const readAndParse = (key, type) => {
+  const value = read(key);
+  if (value === null) return null;
+
+  switch (type) {
+    case 'number':
+      return Number(value);
+    case 'boolean':
+      return value === 'true';
+    case 'string':
+      return String(value);
+    default:
+      return value;
+  }
+};
+
+export const storage = {
+  store,
+  read,
+  readAndParse,
+};
