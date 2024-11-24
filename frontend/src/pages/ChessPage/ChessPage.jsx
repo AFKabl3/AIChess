eb0d817ce03ab10a5a4809815b4318f4aeac4471
thorsjur <thorsj00@hotmail.com>
2024-11-24 22:16:44 +0100

    revert lock

--- frontend/src/pages/ChessPage/ChessPage.jsx
+++ frontend/src/pages/ChessPage/ChessPage.jsx
@@ -7,7 +7,6 @@ import { useChess } from '../../hooks/useChess';
 import { useConfig } from '../../hooks/useConfig';
 import { useMoveHistory } from '../../hooks/useMoveHistory';
 import { formatUciMove, parseArrow } from '../../util/chessUtil';
-import { waitForResponseToast } from '../../util/toasts';
 import { Chat } from './Chat/Chat';
 import { ChessBoardWrapper } from './ChessBoardWrapper/ChessBoardWrapper';
 import { ChessContext } from './ChessContext';
@@ -41,11 +40,6 @@ export const ChessPage = () => {
   const onPlayerMove = async (move, fen) => {
     if (!llmUse) return;
 
-    if (lock) {
-      waitForResponseToast();
-      return;
-    }
-
     const modifyText = addBotChat(`You played ${move}. Evaluating the move ...`);
 
     setLock(true);
@@ -64,11 +58,6 @@ export const ChessPage = () => {
   };
 
   const onSuggestionRequest = async () => {
-    if (lock) {
-      waitForResponseToast();
-      return;
-    }
-
     const modifyText = addBotChat('Suggesting a move ...');
 
     setLock(true);
